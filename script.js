2640488f7f024a1958a13125e87d81faa1f62bb5
Jason Paredes <haloos@me.com>
2019-03-04 09:53:28 -0500

    added username, timeline

---
+++ script.js
@@ -0,0 +1,17 @@
+var datbase = [
+  {
+      username: "jason",
+      password: "secret2"
+  }
+]; 
+
+var newsFeed = [
+  {
+    username: "Tony",
+    timeline: "I'm tired from the weekend"
+  },
+  {
+    username: "Jess",
+    timeline: "No work today!"
+  }
+];
