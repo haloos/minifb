c267ba0db5cd208d5cd7ec9b2aa3a7de9283d6fc
Jason <haloos@me.com>
2019-03-06 10:25:01 -0500

    added database.length

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ var database = [
   {
       username: "jason",
       password: "secret2"
-  }
+  },
+  {
+    username: "sally",
+    password: "321"
+},
+{
+  username: "jill",
+  password: "456"
+},
 ]; 
 
 var newsFeed = [
@@ -16,16 +24,25 @@ var newsFeed = [
   }
 ];
 
-var userNamePromt = prompt("What's your username?");
-var passwordPromt = prompt("What's your password?");
-
-function signIn(user, pass) {
- if (user === database[0].username && 
-    pass === database[0].password) {
-      console.log(newsfeed); 
-    } else {
-      alert("Sorry, wrong username and password!"); 
+function signIn(username, password) {
+  for (var i=0; i < database.length; i++) {
+      if(database[i].username === username &&
+          database[i].password === password) {
+            console.log(newsfeed);
+          } else {
+            alert("Sorry, wrong username and password");
+          }
+  }
+ 
+  // if (user === database[0].username && 
+  //   pass === database[0].password) {
+  //     console.log(newsfeed); 
+  //   } else {
+  //     alert("Sorry, wrong username and password!"); 
     }
-}
+// }
+
+var userNamePromt = prompt("What\'s your username?");
+var passwordPromt = prompt("What\'s your password?");
 
 signIn(userNamePromt, passwordPromt);
